7361578da9776270182dcc78a9c3bc8d8b12b861
Raul Hernandez <ralejandro1999@hotmail.com>
2020-02-06 13:41:23 -0600

    actualizar

--- Tarea01.R
+++ Tarea01.R
@@ -2,6 +2,7 @@ library(repmis)
 conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
 
 mean(conjunto$Altura)
+
 h.media <- subset(conjunto, conjunto$Altura <= mean(conjunto$Altura))
 h.16 <- subset(conjunto, conjunto$Altura <16.5)
 
@@ -9,17 +10,19 @@ vecinos.3 <- subset(conjunto,conjunto$Vecinos <= 3)
 vecinos.4 <- subset(conjunto,conjunto$Vecinos > 4)
 
 DBH.media <- subset(conjunto,conjunto$Diametro < mean(conjunto$Diametro))
-DBH.16 <-subset(conjunto,conjunto$Diametro >16)
+DBH.16 <- subset(conjunto,conjunto$Diametro >16)
 
 
 # especie -----------------------------------------------------------------
 
 cedro_rojo <- subset(conjunto, conjunto$Especie == "c")
-Tsuga_heterofila <- subset(conjunto, conjunto$Especie == "h")
-Douglasia_verde <- subset(conjunto, conjunto$Especie == "f")
+Tsuga_heterofila <- subset(conjunto, conjunto$Especie == "H")
+Douglasia_verde <- subset(conjunto, conjunto$Especie == "F")
+
+HYF <- rbind(Tsuga_heterofila, Douglasia_verde)
 
 
-# cuantas observaciones son menor o igual a 16.9 cm dimaetro --------------
+# cuantas observaciones son menor o iguales a 16.9 cm de diametro ---------
 
 sum(conjunto$Diametro <= 16.9)
 
@@ -27,11 +30,21 @@ sum(conjunto$Diametro <= 16.9)
 
 sum(conjunto$Altura > 18.5)
 
-# Histogramas -------------------------------------------------------------
 
-hist(conjunto$Altura, col="blue", xlab= "H.media", ylab = "H.16", main = "altura")
-hist(conjunto$Vecinos, col="yellow", xlab = "vecino.3", ylab = "vecino.4", main = "vecinos")
-hist(conjunto$Diametro, col = "red", xlab = "DBH.media", ylab = "DBH.16", main = "diametro")
+# histograma --------------------------------------------------------------
+
+hist(conjunto$Altura, col = "blue", xlab = "h.media", ylab = "h.16", main = "altura")
+hist(conjunto$Vecinos, col = "red", xlab = "vecinos.3", ylab = "vecinos.4", main = "vecinos")
+hist(conjunto$Diametro, col = "yellow", xlab = "DBH.media", ylab = "DBH.16", main= "diametro")
+
+# 3 graficas ----------------------------------------------------------------
+
+par(mfrow=c(2,2))
+hist(Tsuga_heterofila$Altura, main = "altura especies H", col = "red", xlab = "altura", ylab = "frecuencia")
+hist(Tsuga_heterofila$Diametro, main = "diametro especie H", col = "blue", xlab = "diametro", ylab = "frecuencia")
+hist(Douglasia_verde$Altura, main = "altura especie F", col = "green", xlab = "altura", ylab = "frecuencia")
+hist(Douglasia_verde$Diametro, main = "diametro especie F", col = "orange", xlab = "diametro", ylab = "frecuencia")
+par(mfrow=c(1,1))
 
 mean(conjunto$Altura)
 sd(conjunto$Altura)
